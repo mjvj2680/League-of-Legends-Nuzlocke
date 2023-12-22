47c5073ed1515d7a2df6e0f1076ddedd7ab9f43c
MagnusTrojahn <magnustrojahn@gmail.com>
2023-12-22 19:04:24 +0100

    init

---
+++ league-nuzlocke/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import matchHistory from './pages/matchhistory';
+
+
+function App() {
+  return (
+    <div className="App">
+     <matchHistory/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ league-nuzlocke/src/pages/matchHistory.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const matchHistory = () => {
+ 
+  return (
+    <div>
+    <p>MatchHistory here</p>
+    </div>
+  );
+}
+
+export default matchHistory;
